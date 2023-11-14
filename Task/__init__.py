fb090c8ee5c337991d25433e4942e9f0f7ffb87b
Ivan <wasserleu@gmail.com>
2023-11-14 22:36:17 +0300

    delete useless line in _init_.py

--- Task/__init__.py
+++ Task/__init__.py
@@ -1,12 +1,3 @@
 import func
 
-# func.add_note()
-# func.add_note()
-# print(func.read_notes())
-# print(func.print_notes())
-# print(func.delete_note('note 2'))
-# print(func.edit('note 11', None, 'TEXT1'))
-# notes = {'notes': [dict(title='note 1', body='text1', date='date1'), dict(title='note 2', body='text2', date='date2')]}
-# notes.get('notes').remove(notes.get('notes')[1])
-# print(notes)
 func.start()
